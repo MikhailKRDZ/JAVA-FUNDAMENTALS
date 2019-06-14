0e6aeb14964eda5b96467824994c46d93bb1c020
Mikhail_Kirdzik <mikhailkirdzik@gmail.com>
2019-06-14 21:24:41 +0300

    Made tasks 1.1 -1.2

--- src/Task1.java
+++ src/Task1.java
@@ -1,3 +1,7 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.util.Scanner;
+
 public class Task1 {
 //    Для успешного обучения из каждого набора заданий достаточно выполнить 1-2.
 //            1.     Приветствовать любого пользователя при вводе его имени через командную строку.
@@ -6,8 +10,33 @@ public class Task1 {
 //            4.     Ввести целые числа как аргументы командной строки, подсчитать их сумму (произведение) и вывести результат на консоль.
 
 public static void main(String[] args) {
+    //    1.     Приветствовать любого пользователя при вводе его имени через командную строку.
+    hello();
+    //    2.     Отобразить в окне консоли аргументы командной строки в обратном порядке.
+    revers();
+
 
 
 }
 
+    private static void hello() {
+        System.out.println(" Hello! Please enter your name:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println("Hello! " + name);
+    }
+
+
+    private static void revers() {
+        String reversLine = "";
+        System.out.print("Введите аргументы (без пробелов): ");
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.next();
+        int positionNumber = line.length()-1;
+        while (positionNumber >= 0) {
+            reversLine = reversLine + line.charAt(positionNumber);
+            positionNumber--;
+        }
+        System.out.print("Обратная последовательность аргументов :'" + reversLine + "'");
+    }
 }
